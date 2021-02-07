d92b5c3b371ab48940f620056ae5bcecc2c78412
Dan Clark <wpu.clark@gmail.com>
2021-02-07 15:29:36 -0500

    Give image_load a home outside of game.py

--- game.py
+++ game.py
@@ -1,29 +1,10 @@
-import os
 import pygame
-from pygame.compat import geterror
-
-main_dir = os.path.split(os.path.abspath(__file__))[0]
-data_dir = os.path.join(main_dir, "data")
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join(data_dir, name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error:
-        print("Cannot load image:", fullname)
-        raise SystemExit(str(geterror()))
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey, pygame.RLEACCEL)
-    return image, image.get_rect()
+from lib.util import load_image
 
 def main():
     pygame.init()
     screen_flags = pygame.FULLSCREEN
     screen = pygame.display.set_mode((0, 0), screen_flags)
-    pygame.display.set_caption("Test Game")
     pygame.mouse.set_visible(0)
 
     background, _ = load_image("main_bg.png")
---
+++ lib/util.py
@@ -0,0 +1,20 @@
+from definitions import ROOT_DIR
+import os
+import pygame
+from pygame.compat import geterror
+
+data_dir = os.path.join(ROOT_DIR, "data")
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join(data_dir, name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error:
+        print("Cannot load image:", fullname)
+        raise SystemExit(str(geterror()))
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, pygame.RLEACCEL)
+    return image, image.get_rect()
